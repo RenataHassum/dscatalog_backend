9fd07c1e89837e773fdbfaea6103d801fdbe9d21
Renata Hassum <renatahassum@gmail.com>
2023-06-14 11:48:36 -0300

    SQL findProductsWithCategories avoiding performance degradation | Homolog Dev OK

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -1,11 +1,26 @@
 package com.devsuperior.dscatalog.repositories;
 
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.devsuperior.dscatalog.entities.Product;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
+            + "(COALESCE(:categories) IS NULL OR cats IN :categories) AND "
+            + "(LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%'))) ")
+    Page<Product> find(List<Category> categories, String name, Pageable pageable);
+
+    @Query("SELECT obj FROM Product obj JOIN FETCH obj.categories WHERE obj IN :products")
+    List<Product> findProductsWithCategories(List<Product> products);
 }
+
+//COALESCE está desatualizado, rever na atualização do projeto qdo estiver disponível total 27 e não 25(correto).
+//findProductsWithCategories (resolução para problema n+1 consultas)
--- backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -8,14 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscatalog.dto.ProductDTO;
@@ -27,10 +20,14 @@ public class ProductResource {
 
 	@Autowired
 	private ProductService service;
-	
+
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
-		Page<ProductDTO> list = service.findAllPaged(pageable);		
+	public ResponseEntity<Page<ProductDTO>> findAll(
+			@RequestParam(value = "categoryId", defaultValue = "0") Long categoryId,
+			@RequestParam(value = "name", defaultValue = "") String name,
+			Pageable pageable) {
+
+		Page<ProductDTO> list = service.findAllPaged(categoryId, name.trim(), pageable);
 		return ResponseEntity.ok().body(list);
 	}
 
@@ -39,7 +36,7 @@ public class ProductResource {
 		ProductDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
 		dto = service.insert(dto);
